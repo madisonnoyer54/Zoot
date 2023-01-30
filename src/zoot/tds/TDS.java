da55c1ebebc9cf92422e946622fca97797111981
Alex <alexmendes74@gmail.com>
2023-01-30 20:02:20 +0100

    classes tds à reprendre

--- src/zoot/tds/TDS.java
+++ src/zoot/tds/TDS.java
@@ -6,8 +6,19 @@ import java.util.Map;
 public class TDS {
     private int compteurDeplace;
     private Map<Entree, Symbole> tableDesSymboles;
+    private static TDS instance = new TDS();
 
     public TDS() {
         this.tableDesSymboles = new HashMap<>();
     }
+
+    public static TDS getInstance() {
+        return instance;
+    }
+
+    public void ajouter (Entree e, Symbole s){
+
+    }
+
+    public void identifier(Entree e){}
 }
