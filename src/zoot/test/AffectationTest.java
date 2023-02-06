2cacb74d794c932ec9c7301248caa5d374c5ab3c
Alex <alexmendes74@gmail.com>
2023-02-06 20:57:08 +0100

    test de la classe affectation

---
+++ src/zoot/test/AffectationTest.java
@@ -0,0 +1,43 @@
+package zoot.test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import zoot.arbre.expressions.ConstanteEntiere;
+import zoot.arbre.expressions.Expression;
+import zoot.arbre.expressions.Idf;
+import zoot.arbre.instructions.Affectation;
+import zoot.tds.Entree;
+import zoot.tds.Symbole;
+import zoot.tds.TDS;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AffectationTest {
+    private Idf idf;
+    private Affectation affect;
+    private Expression express;
+    private Symbole symbole;
+    private Entree entree;
+    @BeforeEach
+    void setUp() {
+        entree = new Entree("var");
+        symbole = new Symbole("entier");
+        TDS.getInstance().getTableDesSymboles().put(entree,symbole);
+        express = new ConstanteEntiere("nb",7);
+        idf = new Idf("var",7);
+        affect = new Affectation(idf.toString(),express,7);
+    }
+
+    @Test
+    void verifier() {
+    }
+
+    @Test
+    void toMIPS() {
+        String code = "# Affectation (" + idf.toString() +" = "+ express.toString()+")\n"+
+                express.toMIPS()+
+                "\tsw $v0, "+ idf.getSymbole().getDeplacement()+"($s7)" +"\n\n";
+        assertEquals(code,affect.toMIPS());
+
+    }
+}
