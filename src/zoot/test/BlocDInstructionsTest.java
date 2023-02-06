7875e66218f96bfa35765d7cda605c347053f3c3
Alex <alexmendes74@gmail.com>
2023-02-06 11:34:22 +0100

    correction du test blocinstruction

--- src/zoot/test/BlocDInstructionsTest.java
+++ src/zoot/test/BlocDInstructionsTest.java
@@ -13,9 +13,19 @@ class BlocDInstructionsTest {
     void toMIPS() {
         BlocDInstructions bloc = new BlocDInstructions(0);
         bloc.toMIPS();
-        String expectedCode =  ".text\n\n"+
-                "main :\n"
-                +"\nend :\n" +
+        String expectedCode =  ".data\n" +
+                "str: 	.asciiz \"\\n\"\n" +
+                "vrai:	.word 1\n" +
+                "faux:	.word 0\n" +
+                "vraiAff:	.asciiz \"vrai\"\n" +
+                "fauxAff:	.asciiz \"faux\"\n" +
+                ".text\n\n" +
+                "main :\n\n" +
+                "# Initialiser $s7 avec $sp\n" +
+                "\tmove $s7,$sp\n" +
+                "# Reserve la place des variables\n" +
+                "\taddi $sp,$sp,0\n\n" +
+                "\nend :\n" +
                 "\tli $v0, 10\n" +
                 "\tsyscall";
         assertEquals(expectedCode, bloc.toMIPS());
