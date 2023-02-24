2917b53ffa35449edaa934c57d93499c54414c20
Alex <alexmendes74@gmail.com>
2023-02-24 23:26:53 +0100

    essai de faire appel fonction (à revoir)

--- src/zoot/arbre/expressions/AppelFonction.java
+++ src/zoot/arbre/expressions/AppelFonction.java
@@ -1,4 +1,41 @@
 package zoot.arbre.expressions;
 
-public class AppelFonction {
+import zoot.tds.Entree;
+import zoot.tds.EntreeFonction;
+import zoot.tds.TDS;
+import zoot.tds.Type;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class AppelFonction extends Expression{
+    private String idf;
+    private String type;
+    private int n;
+    public AppelFonction(int n,String idf) {
+        super(n);
+        this.n=n;
+        this.idf = idf;
+    }
+
+    @Override
+    public void verifier() {
+
+    }
+
+    @Override
+    public String toMIPS() {
+        return null;
+    }
+
+    @Override
+    public Type getType() {
+        //On récupère le type de retour de la fonction
+        return TDS.getInstance().identifier(new EntreeFonction(this.idf,this.n)).getType();
+    }
+
+    @Override
+    public boolean estBool() {
+        return false;
+    }
 }
