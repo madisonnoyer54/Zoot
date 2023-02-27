c9975dd4832cbe5f4e3f0e838b32c8884ce2440f
Alex <alexmendes74@gmail.com>
2023-02-27 09:39:27 +0100

    ajout des classes fonctions et GestionnaireFonctions

--- src/zoot/arbre/fonctions/Fonction.java
+++ src/zoot/arbre/fonctions/Fonction.java
@@ -13,4 +13,12 @@ public class Fonction {
         this.noLigne = noLigne;
         this.arbre = arbre;
     }
+
+    public String toMIPS() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\n\n#Déclaration de la fonction ").append(this.idf).append("\n");
+        sb.append(this.idf).append(": \n").append(this.arbre.toMIPS());
+        sb.append("\tjr $ra"); //On retourne à l'appel de la fonction
+        return sb.toString();
+    }
 }
