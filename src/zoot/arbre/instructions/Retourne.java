f8260f510e599907d59e443bdcaa2b12a11acd1e
Alex <alexmendes74@gmail.com>
2023-03-27 23:01:00 +0200

    modif retourne tomips

--- src/zoot/arbre/instructions/Retourne.java
+++ src/zoot/arbre/instructions/Retourne.java
@@ -32,15 +32,15 @@ public class Retourne extends Instruction{
         String code;
         int nbVar=TDS.getInstance().getTailleZoneVariable(num);
         // depiler s7 et ra
-        code = "\n# Depile de s7 et ra\n"+
-                "\taddi $sp,$sp, "+nbVar+"\n"+ //OK
-                "\tlw $s7, 0($sp)\n" + //~
-                "\taddi $sp, $sp, 4\n" + //OK
-                "\tlw $ra, 0($sp)"+ //OK
+        code = //"\n# Depile de s7 et ra\n"+
+                //"\taddi $sp,$sp, "+nbVar+"\n"+ //OK
+                //"\tlw $s7, 0($sp)\n" + //~
+                //"\taddi $sp, $sp, 4\n" + //OK
+                //"\tlw $ra, 0($sp)"+ //OK
 
 
         "\t#Retour de fonction "+ e.toString()    +
-        "\n\tsw $v0, 0($sp)\n"+ // 12 et pas 0
+        //"\n\tsw $v0, 0($sp)\n"+ // 12 et pas 0
               e.toMIPS()+
         "\tjr $ra\n";
         return code;
