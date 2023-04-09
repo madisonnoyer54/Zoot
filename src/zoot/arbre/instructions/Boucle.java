cc565fb51f2e6938f0fadee4e7d74e6a910cd9f0
madisonnoyer54 <115881298+madisonnoyer54@users.noreply.github.com>
2023-04-09 22:22:07 +0200

    modif

--- src/zoot/arbre/instructions/Boucle.java
+++ src/zoot/arbre/instructions/Boucle.java
@@ -27,7 +27,7 @@ public class Boucle extends Instruction{
     public void verifier() {
         this.e.verifier();
         if(!(this.e.estBool() && this.e != null)){
-            Analyse.getInstance().ajoute(new AnalyseSemantiqueException(noLigne+" : l'expression doit etre de type boolean"));
+       //     Analyse.getInstance().ajoute(new AnalyseSemantiqueException(noLigne+" : l'expression doit etre de type boolean"));
 
         }
         blocDInstructions.verifier();
@@ -59,5 +59,41 @@ public class Boucle extends Instruction{
     @Override
     public boolean estRetourner() {
         return false;
+    }
+
+    @Override
+    public boolean estBoucleOuCondition() {
+        return true;
+    }
+
+    @Override
+    public Boolean contientRetourner(){
+        boolean finale = false;
+        for (Instruction instruction : blocDInstructions.getProgramme()) {
+            if(instruction.estRetourner()){
+                finale =  true;
+            }
+            if(instruction.estBoucleOuCondition()){
+                instruction.contientRetourner();
+            }
+        }
+
+        return  finale;
+    }
+
+    @Override
+    public Instruction getRetourner() {
+        Instruction finale = null;
+
+        for (Instruction instruction :blocDInstructions.getProgramme()) {
+            if (instruction.estRetourner()) {
+                finale = instruction;
+            }
+            if (instruction.estBoucleOuCondition()) {
+                finale = instruction.getRetourner();
+            }
+        }
+
+        return  finale;
     }
 }
